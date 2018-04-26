0a4797094f9da70dd4bfafed96373dda8413fdd5
Dillion Verma <dillionverma@hotmail.com>
2018-04-26 15:24:56 -0700

    add java

---
+++ fib.java
@@ -0,0 +1,11 @@
+class fib {
+  static int fib(int n) {
+    if (n <= 1) return n;
+    return fib(n-1) + fib(n-2);
+  }
+    
+  public static void main (String args[]) {
+    int n = Integer.parseInt(args[0]);
+    System.out.println(fib(n));
+  }
+}
